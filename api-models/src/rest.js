4712a5b431567abf4dc3da93022205a42c103257
Luke Kaalim <lkaalim@nine.com.au>
2021-01-20 11:30:09 +1100

    Add different modify and create args

--- api-models/src/rest.js
+++ api-models/src/rest.js
@@ -7,31 +7,44 @@ const { createAPIResource } = require('./resource');
 const toNull/*: Cast<null>*/ = () => null;
 
 /*::
-export type RESTResource<T: JSONValue, TArgs: JSONValue, TIdentifier: QueryMap, TFilter: QueryMap> = {
-  create: APIResource<T, TArgs, null>,
+export type RESTResource<
+  T: JSONValue,
+  TCreateArgs: JSONValue,
+  TModifyArgs: JSONValue,
+  TIdentifier: QueryMap,
+  TFilter: QueryMap
+> = {
+  create: APIResource<T, TCreateArgs, null>,
   list:   ?APIResource<T[], null, TFilter>,
   get:    APIResource<T, null, TIdentifier>,
-  patch:  APIResource<T, TArgs, TIdentifier>,
+  patch:  APIResource<T, TModifyArgs, TIdentifier>,
   delete: APIResource<null, null, TIdentifier>,
 };
 */
 
-const createRESTResource = /*::<T: JSONValue, TArgs: JSONValue, TIdentifier: QueryMap, TFilter: QueryMap>*/ (
+const createRESTResource = /*::<
+  T: JSONValue,
+  TCreateArgs: JSONValue,
+  TModifyArgs: JSONValue,
+  TIdentifier: QueryMap,
+  TFilter: QueryMap
+>*/ (
   name/*: string*/,
   toResource/*: Cast<T>*/,
-  toArgs/*: Cast<TArgs>*/,
+  toCreateArgs/*: Cast<TCreateArgs>*/,
+  toModifyArgs/*: Cast<TModifyArgs>*/,
   toIdentifier/*: Cast<TIdentifier>*/,
   toFilter/*: ?Cast<TFilter>*/ = null,
-)/*: RESTResource<T, TArgs, TIdentifier, TFilter>*/ => {
+)/*: RESTResource<T, TCreateArgs, TModifyArgs, TIdentifier, TFilter>*/ => {
   const toList = (value) => {
     const array = toArray(value);
     return array.map(toResource);
   };
   return {
-    create: createAPIResource(name, 'POST', toNull, toArgs, toResource, true),
+    create: createAPIResource(name, 'POST', toNull, toCreateArgs, toResource, true),
     list:   toFilter ? createAPIResource(name, 'GET', toFilter, toNull, toList, true) : null,
     get:    createAPIResource(name, 'GET', toIdentifier, toNull, toResource, true),
-    patch:  createAPIResource(name, 'PATCH', toIdentifier, toArgs, toResource, true),
+    patch:  createAPIResource(name, 'PATCH', toIdentifier, toModifyArgs, toResource, true),
     delete: createAPIResource(name, 'DELETE',toIdentifier, toNull, toNull, true),
   }
 };
