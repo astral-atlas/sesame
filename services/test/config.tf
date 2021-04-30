3755376d5912804d86be32e10075d72810302cb6
Luke Kaalim <lukekaalim@gmail.com>
2021-04-30 18:18:32 +1000

    Move configuration to main
    Add backend

---
+++ services/test/config.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+    immutable-elastic-beanstalk = {
+      source = "lukekaalim/immutable-elastic-beanstalk"
+      version = "2.0.4"
+    }
+  }
+  backend "remote" {
+    organization = "astral-atlas"
+
+    workspaces {
+      name = "sesame"
+    }
+  }
+}
+
+provider "immutable-elastic-beanstalk" {
+  region = "ap-southeast-2"
+}
+
+provider "aws" {
+  region = "ap-southeast-2"
+}
