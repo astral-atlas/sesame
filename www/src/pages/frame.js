5ce0e076180656664d4478cf714991e686ab1a43
Luke Kaalim <lukekaalim@gmail.com>
2021-05-04 02:33:04 +1000

    Update models to use WWWMessage for frame comms

--- www/src/pages/frame.js
+++ www/src/pages/frame.js
@@ -1,5 +1,6 @@
 // @flow strict
 /*:: import type { Node } from 'preact'; */
+/*:: import type { WWWMessage } from '@astral-atlas/sesame-models'; */
 import { h } from 'preact';
 import { useEffect } from 'preact/hooks';
 import { AuthorizationForm } from '../components/authorize';
@@ -16,13 +17,21 @@ export const FramePage = ()/*: Node*/ => {
       return;
     const { access } = await client.getSelfUser();
     const { offerProof } = await client.createAccessOfferForSelf();
-    parent.postMessage({ type: 'sesame_access_offer', offerProof, access }, '*');
+    const message/*: WWWMessage*/ = {
+      type: 'sesame-www-offer',
+      offer: offerProof,
+      deviceName: access ? access.grant ? access.grant.deviceName : null : null,
+    };
+    parent.postMessage(message, '*');
   };
   useEffect(() => {
     if (parent === window)
       return;
     window.addEventListener('message', onMessage);
-    parent.postMessage({ type: 'init' }, '*');
+    const message/*: WWWMessage*/ = {
+      type: 'sesame-www-init',
+    };
+    parent.postMessage(message, '*');
     return () => window.removeEventListener(onMessage);
   }, []);
   if (parent === window)
