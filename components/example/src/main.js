8a661c6b928c7fffff12f6ca61d080176451e2ef
Luke Kaalim <lukekaalim@gmail.com>
2021-05-06 00:01:13 +1000

    Update examples

--- components/example/src/main.js
+++ components/example/src/main.js
@@ -2,14 +2,18 @@
 import { h, render } from 'preact';
 import { AuthorizerFrame } from '@astral-atlas/sesame-components';
 
+const App = () => {
+  return h(AuthorizerFrame, {
+    wwwBaseURL: new URL('http://localhost:5000'),
+    onAuthorizeOffer: console.log
+  })
+}
+
 const main = () => {
   const body = document.body;
   if (!body)
     return;
-  render(h(AuthorizerFrame, {
-    wwwBaseURL: new URL('http://localhost:5000'),
-    onAuthorizeOffer: console.log
-  }), body);
+  render(h(App), body);
 };
 
 main();
