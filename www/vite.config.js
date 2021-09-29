caef689683523b5aee6d58eed15dd54669273149
Luke Kaalim <lukekaalim@gmail.com>
2021-09-29 15:23:01 +1000

    Update to vite

--- www/vite.config.js
+++ www/vite.config.js
@@ -1,6 +1,16 @@
 import { resolve } from 'path';
 
 export default {
+  build: {
+    outDir: 'artifacts',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        'frame/login': resolve(__dirname, 'frame/login.html'),
+        'token/login': resolve(__dirname, 'token/login.html'),
+      }
+    }
+  },
   server: {
     port: 8080
   },
