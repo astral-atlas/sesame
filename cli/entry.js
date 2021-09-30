1b373c430d08b2a28cd9fc2e88bf3806a267e545
Luke Kaalim <lukekaalim@gmail.com>
2021-09-30 20:44:17 +1000

    Add cli code

--- cli/entry.js
+++ cli/entry.js
@@ -3,6 +3,9 @@
 import { S3 } from '@aws-sdk/client-s3';
 import { createAWSS3SesameData } from "@astral-atlas/sesame-data";
 import { createLoginProof, encodeProofToken } from "@astral-atlas/sesame-models";
+import generateString from 'crypto-random-string';
+import { v4 as uuid } from 'uuid';
+
 
 const initAWS = async () => {
   const s3 = new S3({ region: 'ap-southeast-2' });
@@ -19,18 +22,24 @@ const initAWS = async () => {
   console.log('done');
 };
 
-const addRootUser = async () => {
+const addUser = async (name) => {
   const s3 = new S3({ region: 'ap-southeast-2' });
 
   const { data } = createAWSS3SesameData(s3, 'sesame-test-data', 'sesame');
+  const user = {
+    id: uuid(),
+    name,
+    adminId: null,
+    creatorAdminId: null
+  };
   const grant = {
     type: 'login',
-    login: "f06f27ef-7274-49d1-b9ef-40ef19015ee8",
-    id: '0'
+    login: user.id,
+    id: uuid()
   };
-  const secret = 'yes';
-  await data.grants.login.set({ partition: "f06f27ef-7274-49d1-b9ef-40ef19015ee8", sort: "0" }, grant)
-  await data.grants.secrets.set({ partition: "f06f27ef-7274-49d1-b9ef-40ef19015ee8", sort: "0" }, secret)
+  const secret = generateString({ length: 32 });
+  await data.grants.login.set({ partition: user.id, sort: grant.id }, grant)
+  await data.grants.secrets.set({ partition: user.id, sort: grant.id }, secret)
   const proof = createLoginProof(grant, secret);
   const token = encodeProofToken(proof)
   console.log(token)
@@ -41,8 +50,8 @@ const entry = async (command, ...subcommands) => {
   switch (command) {
     case 'init-aws':
       return await initAWS();
-    case 'add-root-user':
-      return await addRootUser();
+    case 'add-user':
+      return await addUser(...subcommands);
     default:
       return console.log('🤷');
   }
