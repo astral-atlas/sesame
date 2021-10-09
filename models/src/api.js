9b8b23a0f2a0dd3e264859e7dd6605daf29ed4b1
Luke Kaalim <lukekaalim@gmail.com>
2021-10-09 20:51:36 +1100

    Export API objects to root level

--- models/src/api.js
+++ models/src/api.js
@@ -3,6 +3,9 @@ import * as users from './api/users.js';
 import * as access from './api/access.js';
 
 export * from './api/meta.js';
+export * from './api/access.js';
+export * from './api/users.js';
+
 export const api = {
   ...users,
   ...access,
