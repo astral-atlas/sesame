f6181bf3992fac57666a5f5b583761344b200fc7
Luke Kaalim <lukekaalim@gmail.com>
2021-10-11 01:55:43 +1100

    Update trusted example to use hooks

--- examples/trusted/index.js
+++ examples/trusted/index.js
@@ -1,45 +1,39 @@
 // @flow strict
 import { render, h, } from 'preact';
 import { useState, useEffect } from 'preact/hooks';
-import { AuthorizerFrame } from '@astral-atlas/sesame-components';
+import { useConsumerMessenger } from '@astral-atlas/sesame-components';
 
-import { createClient } from '@astral-atlas/sesame-client';
+import { createSesameSDK } from '@astral-atlas/sesame-client';
 import { createWebClient } from '@lukekaalim/http-client';
 
 const httpClient = createWebClient(fetch);
 const identityURL = new URL('http://sesame.astral-atlas.com');
 
+const serviceProof = {
+  type: 'service',
+  grantId: 'dbd1c10d-14f1-4ae7-ad1b-673c2173a1b2',
+  serviceId: '179a9ebc-677b-4f0a-9d91-cbb497c199f5',
+  secret: 'e3ff393be55de2031ac56f6c99f6b865'
+};
+const sdk = createSesameSDK(new URL(`http://localhost:5543`), httpClient, serviceProof);
+
 const TrustedApp = () => {
   const [name, setName] = useState(null);
   const [token, setToken] = useState(null);
 
-  const [messenger, setMessenger] = useState();
-
-  useEffect(() => {
-    if (!messenger)
-      return;
-    const { remove } = messenger.addUpdateLinkedIdentityListener(onIdentityGrant);
-    return () => {
-      remove();
-    };
-  }, [messenger]);
-
-  const onClick = () => {
-    if (!messenger)
-      return;
-    messenger.send({ type: 'sesame:prompt-link-grant' });
-  }
-
   const onIdentityGrant = async ({ grant, proof, secret, token }) => {
-    const sesameClient = createClient(new URL(`http://localhost:5543`), httpClient, proof);
-    const user = await sesameClient.user.get(grant.identity);
+    const valiatedGrant = await sdk.validateProof(proof);
+    if (!valiatedGrant)
+      throw new Error();
+    const user = await sdk.getUser(valiatedGrant.identity);
     setToken(token);
     setName(user.name);
   };
 
+  const messenger = useConsumerMessenger(identityURL.href, onIdentityGrant);
+
   return [
-    h(AuthorizerFrame, { origin: 'http://localhost:8080', onMessengerLoad: m => setMessenger(m) }),
-    (messenger || null) && h('button', { onClick }, 'Lets log in!'),
+    messenger && h('button', { onClick: () => messenger.send({ type: 'sesame:prompt-link-grant' }) }, 'Lets log in!'),
     name && h('p', {}, `You are logged in as ${name}`),
     token && h('p', {}, [`Your authorization token is: `, h('pre', {}, token)]),
   ];
