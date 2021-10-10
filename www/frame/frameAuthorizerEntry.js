93d11a24db8d5fabf57915ed08e4e684ddc123c5
Luke Kaalim <lukekaalim@gmail.com>
2021-10-11 01:56:03 +1100

    catch and log authorizer page errors

--- www/frame/frameAuthorizerEntry.js
+++ www/frame/frameAuthorizerEntry.js
@@ -8,29 +8,33 @@ import { loadConfigFromURL } from "../src/config.js";
 import { identityStore } from "../src/storage/identity.js";
 
 const main = async () => {
-  const url = new URL(document.location.href);
-  const service = url.searchParams.get("service");
-
-  if (!service)
-    throw new Error(`Missing query param: "service"`);
-
-  const messenger = createProviderMessenger(service, window);
-  const config = await loadConfigFromURL();
-  const httpClient = createWebClient(fetch);
-
-  messenger.addPromptGrantListener(async () => {
-    const identity = identityStore.get();
-    const client = createClient(new URL(config.api.sesame.origin), httpClient, identity && identity.proof);
-
-    if (identity) {
-      const { grant, secret } = await client.grants.link.create(service);
-      const proof = createLinkProof(grant, secret);
-      const token = encodeProofToken(proof);
-      messenger.send({ type: 'sesame:update-link-grant', grant, secret, proof, token });
-    }
-  });
-
-  messenger.send({ type: 'sesame:identity-provider-ready' });
+  try {
+    const url = new URL(document.location.href);
+    const service = url.searchParams.get("service");
+  
+    if (!service)
+      throw new Error(`Missing query param: "service"`);
+  
+    const messenger = createProviderMessenger(service, window);
+    const config = await loadConfigFromURL();
+    const httpClient = createWebClient(fetch);
+  
+    messenger.addPromptGrantListener(async () => {
+      const identity = identityStore.get();
+      const client = createClient(new URL(config.api.sesame.origin), httpClient, identity && identity.proof);
+      
+      if (identity) {
+        const { grant, secret } = await client.grants.link.create(service);
+        const proof = createLinkProof(grant, secret);
+        const token = encodeProofToken(proof);
+        messenger.send({ type: 'sesame:update-link-grant', grant, secret, proof, token });
+      }
+    });
+  
+    messenger.send({ type: 'sesame:identity-provider-ready' });
+  } catch (error) {
+    console.warn(error.message);
+  }
 };
 
 main();
